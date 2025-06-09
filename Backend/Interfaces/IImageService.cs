dbb150fb87c7d165ab2767fbcc60bb3c8e994d21
ArseniiOstapyk <127298192+ArseniiOstapyk@users.noreply.github.com>
2025-06-10 00:08:00 +0200

    Add IImageService

---
+++ Backend/Interfaces/IImageService.cs
@@ -0,0 +1,12 @@
+﻿using Backend.DTO.Image;
+using Backend.Models;
+
+namespace Backend.Interfaces
+{
+    public interface IImageService
+    {
+        Task<Image> UploadImageAsync(UploadImageDto dto);
+        Task<Stream?> GetImageStreamByIdAsync(int id);
+        Task<bool> DeleteImageByIdAsync(int id);
+    }
+}
